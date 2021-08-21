739fd04e5db9023635d475c7708b55e5b303c0da
Eduardo Dias Gusmao <edg@outlook.com>
2021-08-20 21:40:18 -0300

    chore: initial config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
